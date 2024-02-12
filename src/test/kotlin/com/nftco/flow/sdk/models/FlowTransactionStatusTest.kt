d5224c75409ac2c6c163f0e91da33d340289d437
Lea Lobanov <lea.lobanov@icloud.com>
2024-02-12 20:07:40 +0800

    Add unit tests for FlowTransactionStatus

---
+++ src/test/kotlin/com/nftco/flow/sdk/models/FlowTransactionStatusTest.kt
@@ -0,0 +1,25 @@
+package com.nftco.flow.sdk.models
+
+import com.nftco.flow.sdk.FlowTransactionStatus
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+
+class FlowTransactionStatusTest {
+    @Test
+    fun `Test status retrieval by number`() {
+        assertEquals(FlowTransactionStatus.UNKNOWN, FlowTransactionStatus.of(0))
+        assertEquals(FlowTransactionStatus.PENDING, FlowTransactionStatus.of(1))
+        assertEquals(FlowTransactionStatus.FINALIZED, FlowTransactionStatus.of(2))
+        assertEquals(FlowTransactionStatus.EXECUTED, FlowTransactionStatus.of(3))
+        assertEquals(FlowTransactionStatus.SEALED, FlowTransactionStatus.of(4))
+        assertEquals(FlowTransactionStatus.EXPIRED, FlowTransactionStatus.of(5))
+    }
+
+    @Test
+    fun `Test invalid status number`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            FlowTransactionStatus.of(6)
+        }
+    }
+}
