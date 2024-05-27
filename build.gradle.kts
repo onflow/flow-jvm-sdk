b7c8617471c5563e29abf9e19aab4f91d3883e17
Lea Lobanov <lea.lobanov@icloud.com>
2024-05-27 20:42:37 +0900

    Bump logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
 
     implementation("com.github.emilianobonassi:jpbc:2.0.0")
     implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
 }
 
 tasks.withType<KotlinCompile> {
