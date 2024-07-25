ffd1cc72a4c92e67ed434e1aa92328685f7dce96
Tarak Ben Youssef <tarak.benyoussef@dapperlabs.com>
2024-07-24 18:23:41 -0600

    extend key gen and key decoding tests and new test of scalar point mult

--- src/test/kotlin/org/onflow/flow/sdk/crypto/SignTest.kt
+++ src/test/kotlin/org/onflow/flow/sdk/crypto/SignTest.kt
@@ -1,8 +1,10 @@
 package org.onflow.flow.sdk.crypto
 
+import org.bouncycastle.jce.interfaces.ECPrivateKey
 import org.onflow.flow.sdk.HashAlgorithm
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.onflow.flow.sdk.SignatureAlgorithm
 import java.math.BigInteger
 import java.security.Signature
 
@@ -12,23 +14,32 @@ internal class SignTest {
         val keyPair = Crypto.generateKeyPair()
         assertNotNull(keyPair.private)
         assertNotNull(keyPair.public)
+        assertEquals(keyPair.private.publicKey, keyPair.public)
     }
 
     @Test
-    fun `Test generating different key pairs`() {
+    fun `Test key generation is randomized`() {
         val keyPair1 = Crypto.generateKeyPair()
         val keyPair2 = Crypto.generateKeyPair()
-
         assertNotEquals(keyPair1.private, keyPair2.private)
         assertNotEquals(keyPair1.public, keyPair2.public)
     }
 
     @Test
-    fun `Can decode private key`() {
+    fun `Can decode keys correctly`() {
+        // generate key bytes
         val keyPair = Crypto.generateKeyPair()
-        val decodedPrivateKey = Crypto.decodePrivateKey(keyPair.private.hex)
-        assertNotNull(decodedPrivateKey)
-        assertEquals(keyPair.private.hex, decodedPrivateKey.hex)
+        val skBytes = keyPair.private.hex
+        val pkBytes = keyPair.public.hex
+
+        // decode private key into the original one
+        val decodedPrivateKey = Crypto.decodePrivateKey(skBytes)
+        assertEquals(keyPair.private, decodedPrivateKey)
+        assertEquals(keyPair.public, decodedPrivateKey.publicKey)
+
+        // decode public key into the original one
+        val decodedPublicKey = Crypto.decodePublicKey(pkBytes)
+        assertEquals(keyPair.public, decodedPublicKey)
     }
 
     @Test
@@ -36,22 +47,46 @@ internal class SignTest {
         assertThrows(IllegalArgumentException::class.java) {
             Crypto.decodePrivateKey("invalidKey")
         }
+        // TODO: add tests for:
+        // - 0 scalar
+        // - scalar >= N
     }
 
-    @Test
-    fun `Can decode public key`() {
-        val keyPair = Crypto.generateKeyPair()
-        val decodedPublicKey = Crypto.decodePublicKey(keyPair.public.hex)
-        assertNotNull(decodedPublicKey)
-        assertEquals(keyPair.public.hex, decodedPublicKey.hex)
-    }
 
     @Test
     fun `Public key throws exception when invalid`() {
         assertThrows(IllegalArgumentException::class.java) {
             Crypto.decodePublicKey("invalidKey")
         }
+        // TODO: add tests for:
+        // - X or Y not in Z_p
+        // - X and Y in Z_p but point not on curve
+        // - infinity point
     }
+
+    @Test
+    fun `Test derivePublicKey`() { // this is also a sanity test of scalar point multiplication
+        val SK = "6e37a39c31a05181bf77919ace790efd0bdbcaf42b5a52871fc112fceb918c95"
+        val curves = listOf(
+            SignatureAlgorithm.ECDSA_SECP256k1,
+            SignatureAlgorithm.ECDSA_P256,
+        )
+        val expectedPKs = listOf(
+            "36f292f6c287b6e72ca8128465647c7f88730f84ab27a1e934dbd2da753930fa39a09ddcf3d28fb30cc683de3fc725e095ec865c3d41aef6065044cb12b1ff61",
+            "78a80dfe190a6068be8ddf05644c32d2540402ffc682442f6a9eeb96125d86813789f92cf4afabf719aaba79ecec54b27e33a188f83158f6dd15ecb231b49808"
+        )
+
+        curves.forEachIndexed { index, curve ->
+            // decode the private key
+            val sk = Crypto.decodePrivateKey(SK, curve)
+            val k = sk.key
+            // get the public key using the internal scalar point multiplication
+            val pkHex = sk.publicKey.hex
+            assertEquals(expectedPKs[index], pkHex)
+        }
+    }
+
+
 
     @Test
     fun `Get signer`() {
