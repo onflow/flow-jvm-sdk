be7cf19791480a1d3ee5c6e702f5cefbc4152acd
Lea Lobanov <lea.lobanov@icloud.com>
2024-07-26 01:59:02 +0700

    Rebase latest changes

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
 
     implementation("org.slf4j:slf4j-api:1.7.30")
     implementation("ch.qos.logback:logback-classic:1.4.12")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
 }
 
 sourceSets {
