b3820e3d750beefa571812355766cd4a762c59b1
Lea Lobanov <lea.lobanov@icloud.com>
2024-08-20 01:52:45 +0700

    Get network parameters - Kotlin example

---
+++ kotlin-example/src/main/kotlin/org/onflow/examples/kotlin/getNetworkParams/GetNetworkParametersAccessAPIConnector.kt
@@ -0,0 +1,16 @@
+package org.onflow.examples.kotlin.getNetworkParams
+
+import org.onflow.flow.sdk.FlowAccessApi
+import org.onflow.flow.sdk.FlowChainId
+
+internal class GetNetworkParametersAPIConnector(
+    private val accessAPI: FlowAccessApi
+) {
+    fun getNetworkParameters(): FlowChainId {
+        return when (val response = accessAPI.getNetworkParameters()) {
+            is FlowAccessApi.AccessApiCallResponse.Success -> response.data
+            is FlowAccessApi.AccessApiCallResponse.Error -> throw Exception(response.message, response.throwable)
+        }
+    }
+}
+
---
+++ kotlin-example/src/test/kotlin/org/onflow/examples/kotlin/getNetworkParams/GetNetworkParametersAccessAPIConnectorTest.kt
@@ -0,0 +1,32 @@
+package org.onflow.examples.kotlin.getNetworkParams
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.onflow.flow.common.test.FlowEmulatorProjectTest
+import org.onflow.flow.common.test.FlowTestClient
+import org.onflow.flow.sdk.FlowAccessApi
+import org.onflow.flow.sdk.FlowChainId
+
+@FlowEmulatorProjectTest(flowJsonLocation = "../flow/flow.json")
+internal class GetNetworkParametersAPIConnectorTest {
+
+    @FlowTestClient
+    lateinit var accessAPI: FlowAccessApi
+
+    private lateinit var networkParametersConnector: GetNetworkParametersAPIConnector
+
+    @BeforeEach
+    fun setup() {
+        networkParametersConnector = GetNetworkParametersAPIConnector(accessAPI)
+    }
+
+    @Test
+    fun `Can fetch network parameters`() {
+        val networkParams: FlowChainId = networkParametersConnector.getNetworkParameters()
+        assertNotNull(networkParams, "Network parameters should not be null")
+        assertTrue(networkParams.id.isNotEmpty(), "Network parameters should have a valid ID")
+        assertEquals(networkParams, FlowChainId.EMULATOR)
+        assertEquals(networkParams.id, "EMULATOR")
+    }
+}
