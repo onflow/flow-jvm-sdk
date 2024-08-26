d1b28d911801271f90af66d6b108caf86ee942ff
Lea Lobanov <lea.lobanov@icloud.com>
2024-08-27 00:13:49 +0700

    WIP: add key example

---
+++ kotlin-example/src/test/kotlin/org/onflow/examples/kotlin/addKey/AddAccountKeyExampleTest.kt
@@ -0,0 +1,20 @@
+package org.onflow.examples.kotlin.addKey
+
+import org.onflow.examples.kotlin.AccessAPIConnector
+import org.onflow.flow.sdk.*
+import org.onflow.flow.sdk.crypto.PrivateKey
+import org.onflow.flow.sdk.crypto.PublicKey
+
+internal class AddAccountKeyExampleTest(
+    privateKey: PrivateKey,
+    accessApiConnection: FlowAccessApi
+) {
+    private val privateKey = privateKey
+    private val accessAPI = accessApiConnection
+
+    private val connector = AccessAPIConnector(privateKey, accessAPI)
+
+    fun addKeyToAccount(payerAddress: FlowAddress, publicKey: PublicKey): FlowTransactionResult {
+    }
+
+}
