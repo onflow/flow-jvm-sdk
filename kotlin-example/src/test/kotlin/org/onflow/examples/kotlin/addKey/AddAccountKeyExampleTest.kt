0452b2203dd6f97b9554096daffba458b7ae6dfb
Lea Lobanov <lea.lobanov@icloud.com>
2024-08-27 00:17:27 +0700

    WIP: add key example

--- kotlin-example/src/test/kotlin/org/onflow/examples/kotlin/addKey/AddAccountKeyExampleTest.kt
+++ kotlin-example/src/test/kotlin/org/onflow/examples/kotlin/addKey/AddAccountKeyExampleTest.kt
@@ -1,20 +1,33 @@
 package org.onflow.examples.kotlin.addKey
 
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.onflow.examples.kotlin.AccessAPIConnector
+import org.onflow.flow.common.test.FlowEmulatorProjectTest
+import org.onflow.flow.common.test.FlowServiceAccountCredentials
+import org.onflow.flow.common.test.FlowTestClient
+import org.onflow.flow.common.test.TestAccount
 import org.onflow.flow.sdk.*
-import org.onflow.flow.sdk.crypto.PrivateKey
-import org.onflow.flow.sdk.crypto.PublicKey
 
-internal class AddAccountKeyExampleTest(
-    privateKey: PrivateKey,
-    accessApiConnection: FlowAccessApi
-) {
-    private val privateKey = privateKey
-    private val accessAPI = accessApiConnection
+@FlowEmulatorProjectTest(flowJsonLocation = "../flow/flow.json")
+internal class AddAccountKeyExampleTest {
+    @FlowServiceAccountCredentials
+    lateinit var serviceAccount: TestAccount
 
-    private val connector = AccessAPIConnector(privateKey, accessAPI)
+    @FlowTestClient
+    lateinit var accessAPI: FlowAccessApi
 
-    fun addKeyToAccount(payerAddress: FlowAddress, publicKey: PublicKey): FlowTransactionResult {
+    private lateinit var accessAPIConnector: AccessAPIConnector
+    private lateinit var connector: AddAccountKeyExample
+
+    @BeforeEach
+    fun setup() {
+        connector = AddAccountKeyExample(serviceAccount.privateKey, accessAPI)
+        accessAPIConnector = AccessAPIConnector(serviceAccount.privateKey, accessAPI)
+    }
+
+    @Test
+    fun `Can add key to account`() {
     }
 
 }
