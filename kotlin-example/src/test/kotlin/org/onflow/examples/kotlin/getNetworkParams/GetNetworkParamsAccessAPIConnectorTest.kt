f79bb7c1004932ec648688fe9c0c2dcd5d31c86a
Lea Lobanov <lea.lobanov@icloud.com>
2024-11-02 22:30:30 +0900

    Kotlin example

--- kotlin-example/src/test/kotlin/org/onflow/examples/kotlin/getNetworkParams/GetNetworkParamsAccessAPIConnectorTest.kt
+++ kotlin-example/src/test/kotlin/org/onflow/examples/kotlin/getNetworkParams/GetNetworkParamsAccessAPIConnectorTest.kt
@@ -3,13 +3,14 @@ package org.onflow.examples.kotlin.getNetworkParams
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.onflow.examples.kotlin.getProtocolState.GetProtocolStateAccessAPIConnector
 import org.onflow.flow.common.test.FlowEmulatorProjectTest
 import org.onflow.flow.common.test.FlowTestClient
 import org.onflow.flow.sdk.FlowAccessApi
 import org.onflow.flow.sdk.FlowChainId
 
 @FlowEmulatorProjectTest(flowJsonLocation = "../flow/flow.json")
-internal class GetNetworkParametersAccessAPIConnectorTest {
+internal class GetNetworkParamsAccessAPIConnectorTest {
     @FlowTestClient
     lateinit var accessAPI: FlowAccessApi
 
